b99ccb039615405dd4ddef0e977d81c8a718781e
jesusprogramador101109 <jesusitogamer773@gmail.com>
2025-07-22 02:50:33 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function mostrarTarjeta() {
-  document.querySelector("button").style.display = "none";
-  document.getElementById("pastel").style.display = "block";
-  document.getElementById("tarjeta").style.display = "block";
-}

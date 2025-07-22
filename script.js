3fa47aeabffe8db674feabe1b55ca78e4d29f7a0
jesusprogramador101109 <jesusitogamer773@gmail.com>
2025-07-22 02:36:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarTarjeta() {
+  document.querySelector("button").style.display = "none";
+  document.getElementById("pastel").style.display = "block";
+  document.getElementById("tarjeta").style.display = "block";
+}

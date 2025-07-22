6973de2fca57d23b0b1639c0845ae9258c5a1c79
jesusprogramador101109 <jesusitogamer773@gmail.com>
2025-07-22 02:33:38 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function mostrarPastel() {
-  document.querySelector('.btn').style.display = 'none';
-  document.getElementById('cake').style.display = 'block';
-  setTimeout(() => {
-    document.getElementById('card').style.display = 'block';
-  }, 800);
-}
